1acee993da1c64bf2701113372c337fb26e1f5e3
chensiyou <905630790@qq.com.cn>
2019-05-01 11:28:00 +0800

    feat："路由跳转判断是否登录"

--- src/main.js
+++ src/main.js
@@ -6,13 +6,35 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
-import {getRequest} from './utils/api'
-import {postRequest} from './utils/api'
-import {deleteRequest} from './utils/api'
-import {putRequest} from './utils/api'
-Vue.config.productionTip = false
+import {deleteRequest, getRequest, postRequest, putRequest} from './utils/api'
 
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.putRequest = putRequest;
 /* eslint-disable no-new */
+router.beforeEach((to, from, next)=> {
+  if (to.name == 'Login') {
+    next();
+    return;
+  }
+  var name = store.state.user.name;
+  if (name == '未登录') {
+    if (to.meta.requireAuth || to.name == null) {
+      next({path: '/', query: {redirect: to.path}})
+    } else {
+      next();
+    }
+  }else {
+    initMenu(router, store);
+    if(to.path=='/chat'){
+      store.commit("updateMsgList", []);
+      next();
+    }
+  }
+})
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/components/Home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/', name: 'Login', component: Login, hidden: true},
+    {path: 'Home', name: '主页', component: Home, hidden: true}
   ]
 })
