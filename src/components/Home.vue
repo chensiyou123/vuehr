8755de746f6da356e59ce8ccd437cd521c98ee97
chensiyou <905630790@qq.com.cn>
2019-05-02 00:17:10 +0800

    feat："代码先移动过来"

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-container class="home-container">
+      <!--头部信息-->
       <el-header class="home-header">
         <span class="home_title">人事管理系统</span>
         <div style="display: flex;align-items: center;margin-right: 7px">
@@ -21,6 +22,7 @@
       </el-header>
       <el-container>
         <el-aside width="180px" class="home-aside">
+          <!--左侧菜单-->
           <div style="display: flex;justify-content: flex-start;width: 180px;text-align: left;">
             <el-menu style="background: #ececec;width: 180px;" unique-opened router>
               <template v-for="(item,index) in this.routes" v-if="!item.hidden">
@@ -35,6 +37,7 @@
             </el-menu>
           </div>
         </el-aside>
+        <!--主题部分-->
         <el-main>
           <el-breadcrumb separator-class="el-icon-arrow-right">
             <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
@@ -52,38 +55,12 @@
 <script>
   export default{
     mounted: function () {
-//      this.devMsg();
-      this.loadNF();
     },
     methods: {
-      loadNF(){
-        var _this = this;
-        this.getRequest("/chat/sysmsgs").then(resp=> {
-          var isDot = false;
-          resp.data.forEach(msg=> {
-            if (msg.state == 0) {
-              isDot = true;
-            }
-          })
-          _this.$store.commit('toggleNFDot', isDot);
-        })
-      },
       goChat(){
         this.$router.push({path: '/chat'});
       },
-      devMsg(){
-        this.$alert('为了确保所有的小伙伴都能看到完整的数据演示，数据库只开放了查询权限和部分字段的更新权限，其他权限都不具备，完整权限的演示需要大家在自己本地部署后，换一个正常的数据库用户后即可查看，这点请大家悉知!', '友情提示', {
-          confirmButtonText: '确定',
-          callback: action => {
-            this.$notify({
-              title: '重要重要!',
-              type: 'warning',
-              message: '小伙伴们需要注意的是，目前只有权限管理模块完工了，因此这个项目中你无法看到所有的功能，除了权限管理相关的模块。权限管理相关的模块主要有两个，分别是 [系统管理->基础信息设置->权限组] 可以管理角色和资源的关系， [系统管理->操作员管理] 可以管理用户和角色的关系。',
-              duration: 0
-            });
-          }
-        });
-      },
+      //注销登录
       handleCommand(cmd){
         var _this = this;
         if (cmd == 'logout') {
